ec158bd594271ccbd2d24e73f05604221d060287
Max Hoffman <maximilian.wolfgang1@gmail.com>
2020-06-24 23:39:00 -0700

    Fix travis i think

---
+++ scripts/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -xeou pipefail
+
+DIR=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+WORKDIR=$DIR/..
+
+cd $WORKDIR
+
+docker run --rm -it \
+    -v $(pwd):/src \
+    klakegg/hugo:0.73.0 \
+    -p 1313:1313 \
+    build \
+    --source $WORKDIR \
+    --forceSyncStatic \
+    --cleanDestinationDir \
+    -D \
+    --gc
